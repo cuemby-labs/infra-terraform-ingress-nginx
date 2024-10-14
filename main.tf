4bd9c02576d307de0fe1d69d7a041558ac36db0c
Manuel Nuila <manuel.nuila@cuemby.com>
2024-10-14 15:31:46 -0600

    ingress-nginx template update

--- main.tf
+++ main.tf
@@ -5,11 +5,9 @@
 resource "helm_release" "ingress_nginx" {
   name       = var.release_name
   namespace  = var.namespace_name
-
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
   version    = var.chart_version
-
   values     = [file("${path.module}/values.yaml")]
 }
 
